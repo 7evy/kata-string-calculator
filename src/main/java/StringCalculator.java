c4c9783a21e4a0654f0fed5855d2365acac1abd3
Dejan PARIS <parisdejan@gmail.com>
2021-09-04 14:45:38 +0200

    Comments and invalid tests
    Added comments for exceptions, format checks before parsing and tests for invalid formats

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,34 +1,54 @@
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class StringCalculator {
 
-    public int Add(String numbers) throws Exception {
-        if (numbers.isEmpty()) {
+    public int Add(String numberString) throws Exception {
+        if (numberString.isEmpty()) {
             return 0;
         }
 
+        // Check delimiter:
+        // - format is //<delimiter>\n at the beginning,
+        // - only single character delimiters
+        // - numbers are not allowed
         String delimiter;
-        if (numbers.startsWith("//")) {
-            delimiter = String.valueOf(numbers.charAt(2));
-            numbers = numbers.split("\n", 2)[1];
+        if (numberString.matches("//[^[0-9]]\n.*$")) {
+            // Custom delimiter is after // prefix
+            delimiter = String.valueOf(numberString.charAt(2));
+            numberString = numberString.split("\n")[1];
         } else {
+            // Default delimiters: , and \n
             delimiter = "[,\n]";
         }
 
-        String[] numbersArray = numbers.split(delimiter);
-        String[] negatives = Arrays.stream(numbersArray)
-                .filter(s -> s.startsWith("-"))
-                .toArray(String[]::new);
+        // Check String format:
+        // - numbers should be separated by a single delimiter
+        // - starting delimiter is forbidden but trailing is allowed
+        // - negative numbers are considered valid syntax
+        String format = "(-?[0-9]+" + delimiter + ")*(-?[0-9]+)?";
+        if (!numberString.matches(format)) {
+            throw new Exception("Bad format");
+        }
+
+        // Parse the numbers from the String using the delimiter
+        Integer[] numbersArray = Arrays.stream(numberString.split(delimiter))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
+
+        // Check for forbidden negative numbers
+        Integer[] negatives = findNegatives(numbersArray);
         if (negatives.length > 0) {
+            System.out.println(Arrays.toString(negatives));
             throw new Exception("Negatives are not allowed: " + Arrays.toString(negatives));
         }
 
         return Arrays.stream(numbersArray)
-                .map(Integer::parseInt)
                 .reduce(0, Integer::sum);
     }
 
-    public static void main(String[] args) {
+    public Integer[] findNegatives(Integer[] numbersArray) {
+        return Arrays.stream(numbersArray)
+                .filter(n -> n < 0)
+                .toArray(Integer[]::new);
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -9,44 +9,75 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void addEmptyStringShouldBeZero() throws Exception {
+    public void Add_EmptyString_ReturnZero() throws Exception {
         Assertions.assertEquals(calculator.Add(""), 0);
     }
 
     @Test
-    public void addOneNumberShouldReturnIt() throws Exception {
+    public void Add_MissingNumber_ThrowException() {
+        Assertions.assertThrows(Exception.class, () -> calculator.Add("1,,2"));
+    }
+
+    @Test
+    public void Add_SingleNumber_ReturnThatNumber() throws Exception {
         Assertions.assertEquals(calculator.Add("1"), 1);
     }
 
     @Test
-    public void addTwoNumbersShouldReturnSum() throws Exception {
-        Assertions.assertEquals(calculator.Add("1,2"), 3);
-    }
-
-    @Test
-    public void addSeveralNumbersShouldReturnSum() throws Exception {
+    public void Add_SeveralNumbers_ReturnSum() throws Exception {
         Assertions.assertEquals(calculator.Add("1,2,3,4,5"), 15);
     }
 
     @Test
-    public void addNewLineIsValidDelimiter() throws Exception {
+    public void Add_StartingDelimiter_ThrowException() {
+        Assertions.assertThrows(Exception.class, () -> calculator.Add(",1,2"));
+    }
+
+    @Test
+    public void Add_TrailingDelimiter_IsValid() throws Exception {
+        Assertions.assertEquals(calculator.Add("1,2,"), 3);
+    }
+
+    @Test
+    public void Add_NewlineDelimiter_IsValid() throws Exception {
         Assertions.assertEquals(calculator.Add("1,2\n3"), 6);
     }
 
     @Test
-    public void addTestCustomDelimiter() throws Exception {
+    public void Add_InvalidDelimiter_ThrowException() {
+        Assertions.assertThrows(Exception.class, () -> calculator.Add("1/2"));
+    }
+
+    @Test
+    public void Add_CustomDelimiterValidSyntax_IsValid() throws Exception {
         Assertions.assertEquals(calculator.Add("//;\n1;2;3"), 6);
     }
 
     @Test
-    public void addNegativeNumbersThrowException() {
+    public void Add_DefaultDelimiterWithACustomOne_ThrowException() {
+        Assertions.assertThrows(Exception.class, () -> calculator.Add("//;\n1,2\n3"));
+    }
+
+    @Test
+    public void Add_CustomDelimiterBadSyntax_ThrowException() {
+        Assertions.assertThrows(Exception.class, () -> calculator.Add("//;1;2;3"));
+        Assertions.assertThrows(Exception.class, () -> calculator.Add("/;\n1;2;3"));
+    }
+
+    @Test
+    public void Add_NumberAsCustomDelimiter_ThrowException() {
+        Assertions.assertThrows(Exception.class, () -> calculator.Add("//0\n10203"));
+    }
+
+    @Test
+    public void Add_NegativeNumbers_ThrowException() {
         Assertions.assertThrows(Exception.class, () -> calculator.Add("-1"));
     }
 
     @Test
-    public void addNegativeNumbersAllAppearInExceptionMessage() {
+    public void Add_NegativeNumbers_AllNegativesAppearInExceptionMessage() {
         try {
-            calculator.Add("-1, -2");
+            calculator.Add("-1,-2");
         } catch (Exception e) {
             Assertions.assertEquals("Negatives are not allowed: [-1, -2]", e.getMessage());
         }
